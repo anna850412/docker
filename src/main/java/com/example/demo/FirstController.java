4b5297325e79c694e09e528d56838305b0766533
Anna Kolodziejczyk <anna.kolodziejczyk@hycom.pl>
2023-11-15 10:32:30 +0100

    FirstContoller added which prints hello world

--- src/main/java/com/example/demo/FirstController.java
+++ src/main/java/com/example/demo/FirstController.java
@@ -1,13 +1,12 @@
-/*package com.example.demo;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String getString(){
         return "Hello world";
     }
 }
-*/
